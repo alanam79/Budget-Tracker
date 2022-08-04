b7cd2dc7e00b3abc9f00ba59737d3850a605b5f4
Alana <halo2382001@yahoo.com>
2022-08-03 20:28:23 -0400

    got indexdb to collect date offline

--- public/js/idb.js
+++ public/js/idb.js
@@ -7,8 +7,8 @@ const request = indexedDB.open("budget_tracker", 1);
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_entry`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_entry', { autoIncrement: true });
+    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_transaction', { autoIncrement: true });
   };
 
   // upon a successful connection to db
@@ -16,10 +16,10 @@ request.onsuccess = function(event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
   
-    // check if app is online, if yes run uploadEntry() function to send all local db data to api
+    // check if app is online, if yes run uploadtransaction() function to send all local db data to api
     if (navigator.onLine) {
       // we haven't created this yet
-      // uploadEntry();
+      // uploadtransaction();
     }
   };
   
@@ -27,3 +27,15 @@ request.onsuccess = function(event) {
     // log error here
     console.log(event.target.errorCode);
   };
+
+  // This function will be executed if we attempt to submit a new transaction and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    // access the object store for `new_transaction`
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+  
+    // add record to your store with add method
+    transactionObjectStore.add(record);
+  }
